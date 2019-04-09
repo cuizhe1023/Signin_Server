d67599b6a33b5fa1126d41d17f7ca8efe0ae40a6
cuizhe1023 <fight.1214@qq.com>
2019-04-09 10:47:25 +0800

    init project,import maven dependency.

---
+++ src/main/java/com/nuc/signin_server/SigninServerApplication.java
@@ -0,0 +1,13 @@
+package com.nuc.signin_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SigninServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SigninServerApplication.class, args);
+    }
+
+}
